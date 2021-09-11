dc6bcb60cfa19ad2c9ad972195b63de55eac40e6
Preston Harms <prestonharms5@gmail.com>
2021-09-11 14:14:38 -0700

    Update solution.py
    Updated answers

--- solution.py
+++ solution.py
@@ -12,14 +12,14 @@ def welcome_assignment_answers(question):
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "Yes"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
         message = 'NYU Computer Networking'
-        hashed_message = hashlib.md5(message.encode())
+        hashed_message = hashlib.md5(message.encode('utf-8'))
         answer = hashed_message.digest()
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
-    elif question == "What layer of the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
+    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
         answer = 7
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
         answer = 4
@@ -31,5 +31,5 @@ def welcome_assignment_answers(question):
 
 if __name__ == "__main__":
     #use this space to debug and verify that the program works
-    debug_question = "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?"
+    debug_question = "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code"
     print(welcome_assignment_answers(debug_question))
