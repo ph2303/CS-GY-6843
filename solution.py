fc280dfc883ff07a4822174aad061d683c53b952
Preston Harms <prestonharms5@gmail.com>
2021-09-11 08:08:59 -0700

    Update solution.py
    Corrected hashing, gave default answer

--- solution.py
+++ solution.py
@@ -14,18 +14,22 @@ def welcome_assignment_answers(question):
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = "Yes"
     elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
-        answer = hashlib.md5('NYU Computer Networking')
+        message = 'NYU Computer Networking'
+        hashed_message = hashlib.md5(message.encode())
+        answer = hashed_message.digest()
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer of the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
         answer = 7
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
         answer = 4
+    else:
+        answer = "You must ask the correct question"
     return(answer)
 # Complete all the questions.
 
 
 if __name__ == "__main__":
     #use this space to debug and verify that the program works
-    debug_question = "Are encoding and encryption the same? - Yes/No"
+    debug_question = "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?"
     print(welcome_assignment_answers(debug_question))
